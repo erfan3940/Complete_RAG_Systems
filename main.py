9d908195a3702692fba3b2152e2025cc63be1f64
Erfan Gholamnezhad <71720523+erfan3940@users.noreply.github.com>
2026-02-04 17:48:32 +0330

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from langchain.chat_models.base import init_chat_model
-llm2 = init_chat_model(
-    model="gemini-2.5-flash",
-    temperature=0.7
-)   
-response2 = llm2.invoke("پورشه برای کدوم کشوره")
-print(response2)
